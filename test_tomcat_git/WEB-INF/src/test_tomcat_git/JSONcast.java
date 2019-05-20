60f1b82411f1555313b0d22be8b606d202cf9a12
kamin <kamin@DESKTOP-7IMV15N>
2019-05-20 12:20:09 +0900

    h

--- test_tomcat_git/WEB-INF/src/test_tomcat_git/JSONcast.java
+++ test_tomcat_git/WEB-INF/src/test_tomcat_git/JSONcast.java
@@ -1,16 +1,29 @@
 package test_tomcat_git;
 
+import org.json.JSONObject;
+
 public class JSONcast
 {
 	DataBaseConect DBC = new DataBaseConect();
+	JSONObject bord,athand,defhand = new JSONObject();
+
+	void sessionencord()
+	{
+
+	}
+
+	void sessiondecord()
+	{
+
+	}
 
 	void encord()
 	{
-		
+
 	}
 
 	void decord()
 	{
-		
+
 	}
 }
