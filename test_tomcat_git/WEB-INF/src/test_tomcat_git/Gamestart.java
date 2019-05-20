9c18d3362ff6638cc7e39c8afe2256dbf1e5b969
kamin <kamin@DESKTOP-7IMV15N>
2019-05-20 15:53:18 +0900

    何か

--- test_tomcat_git/WEB-INF/src/test_tomcat_git/Gamestart.java
+++ test_tomcat_git/WEB-INF/src/test_tomcat_git/Gamestart.java
@@ -1,7 +1,27 @@
 package test_tomcat_git;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Gamestart
 {
 	Gamemain Gm = new Gamemain();
 	Text tx = new Text();
+
+	void createdirectry()
+	{
+		 try{
+		    	Path path1 = Paths.get("");//相対指定でルームidフォルダ作成
+		    	System.out.println(path1);
+				Files.createDirectory(path1);
+			}
+		    catch (IOException e)
+		    {
+				e.printStackTrace();
+			}
+
+
+	}
 }
