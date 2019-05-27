e09b496cd64199f8b07f90173643a72f9c4b1d0a
user <user@soba>
2019-05-27 16:13:25 +0900

    servlet・Beans 更新

--- test_tomcat_git2/WEB-INF/src/test_tomcat_git/servlet_test.java
+++ test_tomcat_git2/WEB-INF/src/test_tomcat_git/servlet_test.java
@@ -32,22 +32,31 @@ public class servlet_test extends HttpServlet
 
 		String name_val = request.getParameter("name"); //リクエスト内に[name]パラメーターで名前を入れてもらう
 
-		HttpSession session = request.getSession();
+		int[] use_hand = conversion((String[])request.getParameterValues("Use_hand"));
+
+		HttpSession session = request.getSession(false);
 
 
-		String[] userinfo = new String[3];//順番 0 ユーザーID 1 ルームID 2 攻守
+		String[] user_info = new String[3];//順番 0 ユーザーID 1 ルームID 2 攻守
+		int[] user_session = new int[3];
 
 		Gson gson = new Gson();
-		if (session.getAttribute("user_name") == null){
 
-			userinfo = game_start.createdirectry(name_val);
+
+		if (session.getAttribute("user_name") == null)
+		{	session = request.getSession(true);
+
+			user_info = game_start.createdirectry(name_val);
 
 			session.setAttribute("user_name", name_val);
-			session.setAttribute("user_id", userinfo[0]);
+			session.setAttribute("userID", user_info[0]);
+			session.setAttribute("roomID", user_info[1]);
+			session.setAttribute("at_df", user_info[2]);
 
-			ub.setuserID(userinfo[0]);
-			ub.setRoomID(userinfo[1]);
-			ub.setAt_Df(userinfo[2]);
+			ub.setsession(session);
+			ub.setuserID(user_info[0]);
+			ub.setRoomID(user_info[1]);
+			ub.setAt_Df(user_info[2]);
 
 			//JSONを生成
 			response.setContentType("application/json");
@@ -55,10 +64,27 @@ public class servlet_test extends HttpServlet
 			response.getWriter().println(gson.toJson(
 				      Collections.singletonMap("param", gson.toJson(ub))
 				    ));
-
 		}
-		else{
-		    	//game_main.
+		else
+		{
+			user_session[0] =(int) session.getAttribute("userID");
+			user_session[1] =(int) session.getAttribute("roomID");
+			user_session[2] =(int) session.getAttribute("at_df");
+		    //game_main.きっと何かメソッド作ってくれる(user_session);
+			//
 		}
 	}
+	int[] conversion(String[] str)
+	{
+		int[] h_int = new int[str.length];
+		for (int i = 0; i < h_int.length; i++)
+		{
+			h_int[i] = Integer.parseInt(str[i]);
+		}
+
+		return h_int;
+
+	}
+
+
 }
