632a87f23d31dfb355f745dde4fd295af2ba873c
kamin <kamin@DESKTOP-7IMV15N>
2019-05-31 12:43:06 +0900

    555

--- test_tomcat_git2/WEB-INF/src/test_tomcat_git/Text.java
+++ test_tomcat_git2/WEB-INF/src/test_tomcat_git/Text.java
@@ -2,7 +2,9 @@ package test_tomcat_git;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Text
@@ -13,7 +15,7 @@ public class Text
 	FileWriter filewriter;
 
 
-	File field;
+	File file;
 
 	void filewrite()
 	{
@@ -28,21 +30,64 @@ public class Text
 		}
 	}
 
-	void filereader(String room,String number,int line)
+	int[] filereader(String room,String number,int line)
 	{
+		String[] linenumber = new String[10];//配列数は仮設定、各行の情報が入力
+		int[] playerinfo = new int[10];//配列数は仮設定
 
 		int roomid = Integer.parseInt(room);//ルーム番号をintにキャスト
 		int playernumber = Integer.parseInt(number);//プレイヤー番号をintにキャスト
 
+		file = new File("");//roomidとplayernumberを使用してファイルを特定
 
+		//以下テキストファイル読み込み
 		try
 		{
+			br = new BufferedReader(new FileReader(file));
+			String str = br.readLine();
 
+			//while(str != null)
+			for(int i = 0;str != null;i++)
+			{
+				linenumber[i] = str;
+			}
 		}
 
 		catch(Exception e)
 		{
+			//何かしらレスポンスを記述
+		}
 
+		finally
+		{
+			brclose();
+		}
+
+		//以下int配列へキャスト
+		String[] temporary = linenumber[line].split(",");
+
+		for(int i = 0;i<temporary.length;i++)
+		{
+			playerinfo[i] = Integer.parseInt(temporary[i]);
+		}
+
+		return playerinfo;
+	}
+
+	void brclose()
+	{
+		if(br!=null)
+		{
+			try
+			{
+				br.close();
+			}
+
+			catch (IOException e)
+			{
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
 		}
 	}
 }
